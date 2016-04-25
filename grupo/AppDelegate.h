a269d6da7fef3c61e7fe15069873a191494a3ba3
makani20 <Siyaram@kishans-macbook-air.local>
2016-04-25 17:12:28 +0530

    Initial Commit

---
+++ grupo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  grupo
+//
+//  Created by kishan makani on 25/04/16.
+//  Copyright © 2016 kishan makani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ grupo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  grupo
+//
+//  Created by kishan makani on 25/04/16.
+//  Copyright © 2016 kishan makani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
